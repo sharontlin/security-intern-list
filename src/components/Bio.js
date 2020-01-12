47b6b1b179995645baee79fe36e46ef4e47a5521
Sharynne T Lin <sharonlin14@gmail.com>
2020-01-12 14:44:24 -0800

    Remove author pic

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -8,16 +8,6 @@ export default ({ settings }) => (
       marginBottom: rhythm(2.5),
     }}
   >
-    <!--<img
-      src={settings.author_avatar.imgix_url}
-      alt={settings.author_name}
-      style={{
-        marginRight: rhythm(1 / 2),
-        marginBottom: 0,
-        width: rhythm(2),
-        height: rhythm(2),
-      }}
-    />-->
     <div dangerouslySetInnerHTML={{ __html: settings.author_bio }} />
   </div>
 )
