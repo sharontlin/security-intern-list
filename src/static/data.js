0f0ba13563e058de77b50a64b06578b7793f597a
Sharynne T Lin <sharonlin14@gmail.com>
2020-01-12 17:17:09 -0800

    Updated jobs list

--- src/static/data.js
+++ src/static/data.js
@@ -1,13 +1,18 @@
 const companies = [
-    ["Dropbox","https://www.dropbox.com/jobs/listing/1853493?gh_src=5qo0vn1","Security Engineer Internship, Production Infrastructure Security (Summer 2020)"],["Salesforce","https://www.linkedin.com/jobs/view/summer-2020-intern-product-security-engineer-at-salesforce-1674636431/","Summer 2020 Intern - Product Security Engineer"],
-    ["Splunk","https://www.linkedin.com/jobs/view/product-security-intern-summer-2020-at-splunk-1525031427/","Product Security Intern (Summer 2020)"],
     ["Trail of Bits","https://jobs.lever.co/trailofbits/ce1446db-2d83-451e-8a5a-fa9e99ab6581","Internship (Summer 2020)"],
     ["Atlassian","https://jobs.lever.co/atlassian/df7aecfd-b082-4b7c-be90-4bbe0b4ec013","2020 Summer Security Intern"],
     ["Planned Parenthood","https://jobs.lever.co/ppfa/fc6701b9-a924-4740-b3be-ba61c5746e2c","2020 Summer Intern-Security Architecture"],
     ["Quizlet","https://jobs.lever.co/quizlet-2/9041ee74-97fc-4c56-b356-ec5244264c62","Technical Security Intern"],
     ["R3","https://jobs.lever.co/r3.com/ba201ded-13a2-4a54-91a1-1e1a0525e2b7/apply","Security Intern"],
+    ["Dropbox","https://www.dropbox.com/jobs/listing/1853493?gh_src=5qo0vn1","Security Engineer Internship, Production Infrastructure Security (Summer 2020)"],["Salesforce","https://www.linkedin.com/jobs/view/summer-2020-intern-product-security-engineer-at-salesforce-1674636431/","Summer 2020 Intern - Product Security Engineer"],
+    ["Splunk","https://www.linkedin.com/jobs/view/product-security-intern-summer-2020-at-splunk-1525031427/","Product Security Intern (Summer 2020)"],
     ["ManoMano","https://jobs.lever.co/manomano/a7e2583a-eb15-4f70-b1a2-1b964bc42643","Security Engineer (Internship) - M/W"],
-    ["Lawrence Livermore National Lab","https://www.linkedin.com/jobs/view/cybersecurity-summer-internship-at-lawrence-livermore-national-laboratory-1665221934/","Cybersecurity Summer Internship"]
+    ["Lawrence Livermore National Lab","https://www.linkedin.com/jobs/view/cybersecurity-summer-internship-at-lawrence-livermore-national-laboratory-1665221934/","Cybersecurity Summer Internship"],
+    ["Relativity","https://jobs.lever.co/relativity/d4b8cedd-74ad-4293-ad56-621987245dbb/apply","Software Engineer Intern - Security (Summer 2020) - 19-0649"],
+    ["Geotab","https://jobs.lever.co/geotab/dd67c749-aa4b-44de-a82c-550011b9d1e2","Security Applications Intern"],
+    ["Research Innovations","https://jobs.lever.co/researchinnovations.com/b4d2a716-4f30-4605-9e08-b323f72ec450","Cyber Security Intern, Summer 2020"],
+    ["iStox","https://jobs.lever.co/istox/a5a72159-7db8-4671-942c-3762ce44c50d?lever-source=Indeed","Cybersecurity Intern"],
+    ["Ancestry","https://jobs.lever.co/ancestry/caefa949-9ad5-40bd-af15-ce2ba9c299a8","Cybersecurity Incident Response (IR) Analyst, Intern"]
 ]
 
 export default companies
